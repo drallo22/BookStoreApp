dd4e6e293c06db97bd03a02dcae6672d5b44d809
David Rallo <davidrallo28@gmail.com>
2023-12-03 21:37:48 -0500

    Added admin dashboard

--- Models/User.cs
+++ Models/User.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BookStoreApp.Models
 {
 	public class User : IdentityUser
 	{
-	}
+        [NotMapped]
+        public IList<string>? RoleNames { get; set; }
+    }
 }
